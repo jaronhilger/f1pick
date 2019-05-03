2390702f2aef0b983c6df1973c713e6133de48f1
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-05-02 21:52:54 -0400

    COGNITO

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  console.log('path');
-  console.log(path.basename);
-  console.log(path.extname);
-  console.log(path.parse);
+  // console.log('path');
+  // console.log(path.basename);
+  // console.log(path.extname);
+  // console.log(path.parse);
+  console.log("## ENVIRONMENT VARIABLES");
+  console.log(JSON.stringify(process.env, null, 2));
+  console.log("## EVENT");
+  console.log(JSON.stringify(event, null, 2));
 
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
