0dcea57639ee8faf5aed27f3f45d7743a536a166
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-05-02 23:21:03 -0400

    apigw change and added subpages

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ exports.get = function(event, context, callback) {
   console.log(JSON.stringify(event, null, 2));
   console.log("## EVENT PATH");
   console.log(JSON.stringify(event.path, null, 2));
-  console.log("## CONTEXT");
-  console.log(JSON.stringify(context, null, 2));
-  let ParsedJSON = JSON.parse(JSON.stringify(event));
-  console.log("## REQUEST CONTEXT");
-  console.log(ParsedJSON.requestContext);
+  // console.log("## CONTEXT");
+  // console.log(JSON.stringify(context, null, 2));
+  // let ParsedJSON = JSON.parse(JSON.stringify(event));
+  // console.log("## REQUEST CONTEXT");
+  // console.log(ParsedJSON.requestContext);
   // console.log("## REQUEST CONTEXT PATH");
   // if (ParsedJSON.requestContext.path) {console.log(ParsedJSON.requestContext.path)};
 
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
+  // var contents = fs.readFileSync(`public${path.sep}index.html`);
+  var contents = fs.readFileSync(`public${event.path}.html`);
   var result = {
     statusCode: 200,
     body: contents.toString(),
