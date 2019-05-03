b8217cf8a7972047d6922c9f0faf04a9b078836a
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-05-02 22:47:02 -0400

    added path back

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  console.log("## ENVIRONMENT VARIABLES");
-  console.log(JSON.stringify(process.env, null, 2));
+  // console.log("## ENVIRONMENT VARIABLES");
+  // console.log(JSON.stringify(process.env, null, 2));
   console.log("## EVENT");
   console.log(JSON.stringify(event, null, 2));
   console.log("## CONTEXT");
@@ -13,8 +13,8 @@ exports.get = function(event, context, callback) {
   let ParsedJSON = JSON.parse(JSON.stringify(event));
   console.log("## REQUEST CONTEXT");
   console.log(ParsedJSON.requestContext);
-  // console.log("## REQUEST CONTEXT PATH");
-  // if (ParsedJSON.requestContext.path) {console.log(ParsedJSON.requestContext.path)};
+  console.log("## REQUEST CONTEXT PATH");
+  if (ParsedJSON.requestContext.path) {console.log(ParsedJSON.requestContext.path)};
 
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
