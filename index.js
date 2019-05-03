7ebfd4ab3aa810f67898e61f73584d4ca90a2dcf
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-05-02 22:07:48 -0400

    cognito fix?

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var fs = require('fs');
 var path = require('path');
 
 exports.get = function(event, context, callback) {
-  // console.log('path');
-  // console.log(path.basename);
-  // console.log(path.extname);
-  // console.log(path.parse);
   console.log("## ENVIRONMENT VARIABLES");
   console.log(JSON.stringify(process.env, null, 2));
   console.log("## EVENT");
   console.log(JSON.stringify(event, null, 2));
+  console.log("## CONTEXT");
+  console.log(JSON.stringify(context, null, 2));
+  let ParsedJSON = JSON.parse(JSON.stringify(event));
+  console.log("## REQUEST CONTEXT PATH");
+  console.log(JSON.stringify(ParsedJSON.requestContext.path));
 
   var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
