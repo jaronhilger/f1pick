48bf374c149d39866a026b56b6831bb5c29eb530
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-08-18 01:16:31 -0400

    updated html and css to use bootstrap.  Adjusted the sign-in page.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-#F1 Project
+F1 Project
 ---
 The goal is to host an app where players can make their picks for the F1 season and the application will run the pool.
 
-##MVP definition
+MVP definition
 ---
     1. Users can use their email address and password to log in to the site.  You cannot access any data or services from the site without logging in
     2. Users can View results for all previous races.  Race results are made available within two hours of race completion.
@@ -12,7 +12,7 @@ The goal is to host an app where players can make their picks for the F1 season 
     6. Users are notified 7 days in advance of the race start time and 24 hours before 1st practice starts
     7. Users are sent a link to the race results when they are avaialble
 
-##AWS needs
+AWS needs
 ---
     - Dynamo Table for user data and race data
     - Lambda function(s) and API gateway to serve webpages and data
@@ -20,10 +20,15 @@ The goal is to host an app where players can make their picks for the F1 season 
     - Cognito for user auth info (possibly their data too)
     - AWS Step function of Cloudwatch Events for scheduled events (like updates, data changes, and emails)
 
-##File List
+Todo
+---
+1. update the aws ADK and make sure my files are up to date.
+1. Work on accessing data.
+
+File List
 -----------
 * README.md - this file
 * buildspec.yml - used by AWS CodeBuild to package application for deployment to AWS Lambda
 * index.js - contains the Node.js code for the web service
 * template.yml - contains the AWS Serverless Application Model (AWS SAM) used by AWS CloudFormation to deploy
-* tests/ - this directory contains unit tests for your application
+* tests/ - this directory contains unit tests
--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -1,12 +1,30 @@
 document.getElementById("header").innerHTML = ' \
-    <nav class="website-nav"> \
-        <ul> \
-            <li><a class="home-link"href="./picks.html">Picks</a></li> \
-            <li><a href="./standings.html">Standings</a></li> \
-            <li><a href="./index.html" onclick="signOut()">Log out</a></li> \
-        </ul> \
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top"> \
+        <a class="navbar-brand" href="#">F1 Pickem</a> \
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation"> \
+            <span class="navbar-toggler-icon"></span> \
+        </button> \
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault"> \
+            <ul class="navbar-nav mr-auto">\
+                <li class="nav-item">\
+                    <a class="nav-link" href="./picks.html">Picks<span class="sr-only">(current)</span></a>\
+                </li>\
+                <li class="nav-item">\
+                    <a class="nav-link" href="./standings.html">Standings</a>\
+                </li>\
+            </ul>\
+        </div>\
+        <div id="logInOrOut">\
+        </div>\
     </nav>'
 
+// <nav class="website-nav"> \
+    //     <ul> \
+    //         <li><a class="home-link"href="./picks.html">Picks</a></li> \
+    //         <li><a href="./standings.html">Standings</a></li> \
+    //         <li><a href="./index.html" onclick="signOut()">Log out</a></li> \
+    //     </ul> \
+    // </nav>'
 document.getElementById("footer").innerHTML = '<p class="footer-contents">Let\'s do this!</p>'
 
 var homePage = "index.html"
@@ -64,6 +82,7 @@ window.onload = function(){
                 return;
             }
             console.log('session validity: ' + session.isValid());
+            document.getElementById("logInOrOut").innerHTML = '<a class="nav-link" href="./index.html" onclick="signOut()">Log Out</a>'
             //Set the profile info
             cognitoUser.getUserAttributes(function(err, result) {
                 if (err) {
@@ -77,6 +96,12 @@ window.onload = function(){
     } else {
         console.log('No one logged in');
         console.log(location.href);
+        document.getElementById("logInOrOut").innerHTML = '\
+        <form class="form-inline my-2 my-lg-0"> \
+            <input class="form-control mr-sm-2" type="text" id="inputUsername"  placeholder="Email address" name="username" required autofocus>\
+            <input class="form-control mr-sm-2" type="password" id="inputPassword"  placeholder="Password" name="password" required>\
+            <button class="btn btn-secondary my-2 my-sm-0" type="button" onclick="signInButton()">Sign in</button>\
+        </form>'
         if (location.href.includes(homePage)) {
             console.log ('You are on the login page.')
         } else {
