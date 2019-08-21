da497e656ab4bd3b248a9f18c517e3af15ae61d3
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-08-21 00:40:53 -0400

    implemented check for element ID so that the API call only runs on the Picks page.

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -26,41 +26,24 @@ var apiEndpoint = 'https://fu9wdatrml.execute-api.us-east-1.amazonaws.com/Prod/'
 
 // https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/
 const app = document.getElementById('helloWorld')
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
-app.appendChild(container)
-var request = new XMLHttpRequest()
-request.open('GET', apiEndpoint, true)
-// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
-
-      const h1 = document.createElement('h1')
-    //   h1.textContent = movie.title
-      h1.textContent = 'title'
-
-      const p = document.createElement('p')
-    //   movie.description = movie.description.substring(0, 300)
-      p.textContent = movie.title
-    //   p.textContent = `${movie.description}...`
-
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Gah, it's not working!`
-    app.appendChild(errorMessage)
-  }
+if(app){
+    var request = new XMLHttpRequest();
+    request.open('GET', apiEndpoint, true);
+    request.onload = function() {
+        var responseData = this.response
+        if (request.status >= 200 && request.status < 400) {
+            const p = document.createElement('p')
+            p.textContent = responseData
+            app.appendChild(p)
+        } else {
+            const errorMessage = document.createElement('marquee')
+            errorMessage.textContent = `Gah, it's not working!`
+            app.appendChild(errorMessage)
+        }
+    }
+    request.send()
 }
-request.send()
-// End https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/ Example
+//end data access
 
 var data = { 
     UserPoolId : _config.cognito.userPoolId,
