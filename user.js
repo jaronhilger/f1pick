62df6c7f82ce2160c7d852e272e119f3cdf5a07f
jaron-hilger <jaron.hilger@karauctionservices.com>
2019-09-06 20:21:27 -0400

    some updates to get the thing to deploy again

--- user.js
+++ user.js
@@ -1,31 +1,8 @@
 'use strict';
 
-// var fs = require('fs');
-// var path = require('path');
-
 exports.get = function(event, context, callback) {
-  // console.log("## ENVIRONMENT VARIABLES");
-  // console.log(JSON.stringify(process.env, null, 2));
   console.log("## EVENT");
-  console.log(JSON.stringify(event, null, 2));
-  // console.log("## EVENT PATH");
-  // console.log(JSON.stringify(event.path, null, 2));
-  // console.log("## CONTEXT");
-  // console.log(JSON.stringify(context, null, 2));
-  // let ParsedJSON = JSON.parse(JSON.stringify(event));
-  // console.log("## REQUEST CONTEXT");
-  // console.log(ParsedJSON.requestContext);
-  // console.log("## REQUEST CONTEXT PATH");
-  // if (ParsedJSON.requestContext.path) {console.log(ParsedJSON.requestContext.path)};
-
-  // var contents = fs.readFileSync(`public${path.sep}index.html`);
-  // let filePath = ''
-  // if (event.path === '/') {
-  //     filePath = `public${path.sep}index2.html`
-  // } else {filePath = `public${event.path}.html`};
-  // console.log(filePath);
-  // var contents = fs.readFileSync(filePath);
-  
+  console.log(JSON.stringify(event, null, 2));  
   var result = {
     statusCode: 200,
     body: JSON.stringify(event, null, 2),
